128549646ca04f3effbabdfd06aeb48ea48d2d24
Matthias Kunz <matthias.kunz@web.de>
2019-12-13 15:18:45 +0100

    update

---
+++ R/tree1.R
@@ -0,0 +1,11 @@
+#' tree1
+#'
+#' A point cloud of a single tree
+#'
+#' @format A data frame with n rows and 3 variables:
+#' \describe{
+#'   \item{x}{x coordinate}
+#'   \item{y}{y coordinate}
+#'   \item{z}{z coordinate}
+#' }
+"tree1"
