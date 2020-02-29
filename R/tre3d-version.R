9273e0ee07651d34d6bb8263c927e5e30a50212c
unknown <matthias.kunz@web.de>
2020-02-29 12:21:43 +0100

    tre3d_version

---
+++ R/tre3d-version.R
@@ -0,0 +1,9 @@
+#' tre3d version information
+#' @author Matthias Kunz, last updated: 29.02.2020
+#' @description Gives information of the tre3d version and changelog
+#' @export
+#' @examples
+#' tre3d_version()
+tre3d_version <- function(){
+  cat(crayon::bold(crayon::blue("\ntre3d version 0.9 'Pretty Pinguin', last updated 29.02.2020\n")))
+}
